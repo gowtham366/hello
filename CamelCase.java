61b6d87d7db29cb336199afb7b1b9359fe9ddc57
ShyluVishnu <vshylu26@gmail.com>
2017-06-08 12:09:17 +0530

    Create CamelCase.java

---
+++ CamelCase.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Scanner;
+class CamelCase
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        String str=s.nextLine();
+        StringBuilder sb=new StringBuilder();
+        String delimiter="'.,;!-/ ";
+        boolean b=true;
+        
+        for(char c:str.toCharArray())
+        {
+            c=(b)?Character.toUpperCase(c):Character.toLowerCase(c);
+            b=(delimiter.indexOf((int)c)>=0);
+            sb.append(c);
+        }
+        str=sb.toString();
+        System.out.println(str);
+    }
+}
