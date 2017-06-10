f19bf6abe2cac01c30b627715fb9c9bc97a535ce
ShyluVishnu <vshylu26@gmail.com>
2017-06-10 10:03:02 +0530

    Create reversewords.java

---
+++ reversewords.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+class reversewords
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String s1=s.nextLine();
+String[] s2=(s1.split(" "));
+for(int i=s2.length-1;i>=0;i--)
+{
+System.out.print(" "+s2[i]);
+}
+}
+}
